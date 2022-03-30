eaec0498b3c50b53ad57bec69a29b8d851aa46bb
Ryley Livingston <79718439+RyleyLivingston@users.noreply.github.com>
2022-03-29 21:01:18 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import styles from './Footer.module.css'
-
+font-size: 15px;
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+        
         Ryley Livingston 2022
       </footer>
     </>
